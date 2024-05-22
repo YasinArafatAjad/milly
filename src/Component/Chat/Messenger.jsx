169efdfdf3d0e18f5ce64e495f926ccb939c922a
yasinarafatajad <yasinarafatazad173@gmail.com>
2024-05-22 12:06:27 +0600

    Data Update

--- src/Component/Chat/Messenger.jsx
+++ src/Component/Chat/Messenger.jsx
@@ -1,12 +1,29 @@
+// import React from 'react';
+// import { CustomChat, FacebookProvider } from 'react-facebook';
+
+// const Messenger = () => {
+//     return (
+//         <>
+//             <FacebookProvider appId="469331782335612" chatSupport>
+//                 <CustomChat pageId="209309862270894" minimized={true}/>
+//             </FacebookProvider> 
+//         </>
+//     );
+// };
+
+// export default Messenger;
+
+
 import React from 'react';
-import { CustomChat, FacebookProvider } from 'react-facebook';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const Messenger = () => {
     return (
         <>
-            <FacebookProvider appId="469331782335612" chatSupport>
-                <CustomChat pageId="209309862270894" minimized={true}/>
-            </FacebookProvider> 
+            <MessengerCustomerChat
+                pageId="<209309862270894>"
+                appId="<469331782335612>"
+            />
         </>
     );
 };
